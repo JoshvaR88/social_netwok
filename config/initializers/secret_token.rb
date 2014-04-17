b5c901831a22a3df2b5bff0b171523b40859a678
Joshua <joshua@usaweb-desktop.(none)>
2014-04-17 18:55:21 +0530

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialNetwork::Application.config.secret_token = '6e2233600ce37b969c9b611b8384e431010b78b93d7510e4643ba10c20231d72d07c293e782427e58197065047e9a0d39a6f88e7e40c889e50d15f4209d57bf6'
---
+++ config/initializers/social_stream-presence.rb
@@ -0,0 +1,44 @@
+SocialStream::Presence.setup do |config| 
+  #Configures Web Server Domain served by XMPP Server
+  config.domain = "localhost"
+  #Configures Bosh Service Path
+  #config.bosh_service = "http://xmpp-proxy/http-bind"
+  #Configures Authentication Method: "cookie" or "password"
+  config.auth_method = "cookie"
+  #Configures XMPP Server Password
+  config.xmpp_server_password = "6d6d2ce684bca16dc2335d381b5d0c91"
+  #Uncomment to enable REST API Security
+  #config.secure_rest_api = true
+  #Remote or local mode
+  config.remote_xmpp_server = false
+  #Scripts path to execute ejabberd scripts: local or remote
+  config.scripts_path = "/scripts_path"
+  #Ejabberd module path in the xmpp server
+  config.ejabberd_module_path = "/usr/lib/ejabberd/ebin"
+  #Uncomment to enable Social Stream Presence
+  #config.enable = true
+  
+  #Parameters for remote mode
+  #SSH Login
+  #config.ssh_domain = "domain"
+  #config.ssh_user = "login"
+  #Comment to allow SSH authentication with key instead of password
+  #config.ssh_password= "password"
+
+  #Configure to use a Xmpp Client on Rails App
+  #Username of the the Social Stream Admin sid
+  config.social_stream_presence_username = "social_stream-presence"
+  #Configures Social Stream Rails App Password
+  config.password = "f018e907dcecd78449c44c32fe98c1c1861930d9b456f933a0a48fb3f52d92c6"
+  
+  #OpenTok settings (Only for videochat)
+  #Replace with your OpenTok API key.
+  #config.opentok_api_key = ""
+  #Replace with your OpenTok API secret.
+  #config.opentok_api_secret = ""
+  
+  #Multiplayer Games Service
+  #Uncomment to enable Multiplayer Games Service
+  #config.games = true
+
+end
